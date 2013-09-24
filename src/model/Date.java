b6177f8328524260c3ec19b148b8915867331ebd
DanielLC <daniel_l_c@yahoo.com>
2013-09-23 18:05:47 -0600

    Made that Date and ValidDate classes, along with some basic Javadoc.

---
+++ src/model/Date.java
@@ -0,0 +1,31 @@
+/**
+ * @author Daniel Carrier
+ *
+ */
+
+public class Date implements Comparable<Date> {
+	
+	/**
+	 * @param day	Day of the month
+	 * @param month	Month of the year
+	 * @param year	Year AD
+	 */
+	public Date(int day, int month, int year) {}
+	
+	/**
+	 * Returns today.
+	 */
+	public Date() {}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+
+	@Override
+	public int compareTo(Date arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+}
---
+++ src/model/ValidDate.java
@@ -0,0 +1,25 @@
+import java.util.zip.DataFormatException;
+
+/**
+ * @author Daniel Carrier
+ *
+ */
+public class ValidDate extends Date {
+
+	/**
+	 * @param day		Day of the month
+	 * @param month		Month of the year
+	 * @param year		Year AD
+	 * @throws DataFormatException	If it's before January 1st, 2000, or it's in the future
+	 */
+	public ValidDate(int day, int month, int year) throws DataFormatException {
+		super(day, month, year);
+		if(this.compareTo(new Date(1,1,2000)) == -1) {
+			throw new DataFormatException("Error: Date should not be before January 1st, 2000.");
+		}
+		if(this.compareTo(new Date()) == 1) {
+			throw new DataFormatException("Error: Date should not be in the future.");
+		}
+	}
+
+}
