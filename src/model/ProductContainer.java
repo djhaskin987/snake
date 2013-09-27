084681936c8f08fb4fd546fc2e92338989b682bf
DanielLC <daniel_l_c@yahoo.com>
2013-09-26 21:57:49 -0600

    Added ProductContainer and ProductItems.
    I had trouble with IProductContainer, so I gave up on implementing it.
    Also, I'm confused as to if I'm supposed to be making ProductItems use
    IProducts and IItems or just Products and Items.

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -1,87 +1,43 @@
 package model;
+import java.util.Collection;
 
-import java.util.List;
 /**
- * A super class for StorageUnits and ProductGroups that contains
- * shared methods and invariants
- * @author Kevin
+ * @author Daniel Carrier
  *
  */
-public class ProductContainer  implements IProductContainer{
-	private NonEmptyString name;
+public abstract class ProductContainer {
+	private String name;
 	private ProductItems productItems;
-	private ProductGroups productGroups;
-	@Override
-	public String getUnit() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public Quantity getThreeMonthSupply() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<Product> getProducts() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String getProductGroupName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public List<Item> getItems(String productName) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public void removeItem(Barcode barcode) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void addItem(Item item) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void addProductGroup(ProductGroup productGroup) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void deleteProductContainer(String name) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void setProductContainer(String name,
-			ProductContainer productContainer) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void transferItem(Item item, ProductContainer newProductContainer) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public void transferProduct(Product product,
-			ProductContainer newProductContainer) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public IProductContainer whoHasProduct(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public IProductContainer getParent() {
-		// TODO Auto-generated method stub
-		return null;
+	private Collection<ProductGroup> productGroups;
+
+	public String getName() {
+		return name;
 	}
 	
+	public abstract Collection<Product> getProducts();
+	
+	//This is what you'd use for a StorageUnit.
+	/*public Collection<Product> getProducts() {
+		ArrayList<Product> products = new ArrayList<Product>(productItems.getProducts());
+		for(ProductGroup productGroup : productGroups) {
+			products.addAll(productGroup.getProducts());
+		}
+		return products;
+	}*/
+	
+	public Collection<Item> getItems() {
+		return productItems.getItems();
+	}
+	
+	public void add(Item item) {
+		Product product = item.getProduct();
+		for(ProductGroup productGroup : productGroups) {
+			if(productGroup.contains(product)) {
+				productGroup.add(item);
+				return;
+			}
+		}
+		productItems.addItem(item);
+		return;
+	}
 }
--- src/model/ProductItems.java
+++ src/model/ProductItems.java
@@ -1,30 +1,80 @@
 package model;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Map;
 
 /**
- * This is a wrapper class that combines all Products
- * and Items into a single object to help keep
- * them linked together throughout.
- * @author Kevin
+ * @author Daniel Carrier
  *
  */
-public class ProductItems implements Serializable{
-	private Map<Product, List<Item>> productItems;
-
-	public ProductItems(Map<Product, List<Item>> productItems) {
-		this.productItems = new HashMap<Product, List<Item>>();
+public class ProductItems {
+	private Map<Product, Collection<Item>> map;
+	
+	/**
+	 * @return	A collection of all the products
+	 */
+	public Collection<Product> getProducts() {
+		return map.keySet();
+	}
+	
+	/**
+	 * @return	A collection of all the items
+	 */
+	public Collection<Item> getItems() {
+		ArrayList<Item> out = new ArrayList<Item>();
+		for(Collection<Item> itemList : map.values()) {
+			out.addAll(itemList);
+		}
+		return out;
+	}
+	
+	/**
+	 * @param item	Item to be added
+	 */
+	public void addItem(Item item) {	//TODO: Should I check if the item is already in the list?
+		Product product = item.getProduct();
+		Collection<Item> itemList = map.get(product);
+		if(itemList == null) {
+			itemList = new ArrayList<Item>();
+			itemList.add(item);
+			map.put(product, itemList);
+		} else {
+			itemList.add(item);
+		}
+	}
+	
+	/**
+	 * @param item	Item to be removed. Automatically removes the product if that's the last item.
+	 */
+	public void removeItem(Item item) {
+		Product product = item.getProduct();
+		Collection<Item> itemList = map.get(product);
+		if(itemList.size() == 1) {
+			map.remove(product);
+		} else {
+			itemList.remove(item);
+		}
 	}
 
-	public Map<Product, List<Item>> getProductItems() {
-		return productItems;
+	/**
+	 * @param product	The product to check if this ProductItem contains.
+	 * @return	True if this ProductItems contains a member of this product. False otherwise.
+	 */
+	public boolean contains(Product product) {
+		return map.containsKey(product);
 	}
-
-	public void setProductItems(Map<Product, List<Item>> productItems) {
-		this.productItems = productItems;
+	
+	/**
+	 * @param item	The item to check if this ProductItem contains.
+	 * @return	True if this ProductItems contains this item. False otherwise.
+	 */
+	public boolean contains(Item item) {
+		Product product = item.getProduct();
+		Collection<Item> itemList = map.get(product);
+		if(itemList == null) {
+			return false;
+		} else {
+			return itemList.contains(item);
+		}
 	}
-
 }
