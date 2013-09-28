80edeec22de2d8e1d4a78163f775775ef94090dd
Daniel Haskin <djhaskin987@gmail.com>
2013-09-28 14:21:41 -0600

    Move tests to the proper place

--- src/model/IProduct.java
+++ src/model/IProduct.java
@@ -9,39 +9,39 @@ import java.util.List;
  *
  */
 public interface IProduct extends Serializable {
-	
+
 	/**
 	 * This must be a NonEmptyString
 	 * @param description of the Product
 	 */
 	public void setDescription(NonEmptyString description);
-	
+
 	/**
 	 * Must be a positive Integer
 	 * @param months
 	 */
 	public void setShelfLife(Integer months);
-	
+
 	/**
 	 * Must be a non-negative Integer
 	 * @param count
 	 */
 	public void setThreeMonthSupply(Integer count);
-	
+
 	/**
 	 * This is the first time this Product was added to the ProductCollection
 	 * @return A ValidDate of when the product was added
 	 */
 	public ValidDate getCreateDate();
-	
+
 	/**
-	 * 
+	 *
 	 * @return Integer of how many Items you will need for a Three month supply of this Product
 	 */
 	public Integer getThreeMonthSupply();
-	
+
 	/**
-	 * 
+	 *
 	 * @return List of all Product Containers that contain this Product
 	 */
 	public List<ProductContainer> getContainers();
--- test-src/model/ValidDateTest.java
+++ test-src/model/ValidDateTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.*;
 import java.util.Calendar;
 import org.junit.Test;
 
-
 /**
  * @author Daniel Carrier
  *
@@ -23,7 +22,7 @@ public class ValidDateTest {
 				new ValidDate(12,31,1999);
 				fail("Failed to throw an exception for 12/31/1999.");
 			} catch (InvalidHITDateException e) {
-			}
+            }
 			try {
 				Date today = new Date();
 				new ValidDate(today.getMonth(), today.getDay(), today.getYear());
@@ -37,16 +36,16 @@ public class ValidDateTest {
 			} catch (DateDoesNotExistException e) {
 				try {
 					tomorrow = new Date(today.getMonth()+1, 1, today.getYear());
-				} catch (DateDoesNotExistException e1) {	
-					tomorrow = new Date(1, 1, today.getYear()+1);
-				}
-			}
-			try {
-				new ValidDate(tomorrow.getMonth(), tomorrow.getDay(), tomorrow.getYear());
-				fail("Failed to throw an exception for next year.");
-			} catch (InvalidHITDateException e) {
-			}
-		} catch (DateDoesNotExistException e) {
-		}
-	}
+                    } catch (DateDoesNotExistException e1) {
+                tomorrow = new Date(1, 1, today.getYear()+1);
+            }
+        }
+        try {
+            new ValidDate(tomorrow.getMonth(), tomorrow.getDay(), tomorrow.getYear());
+            fail("Failed to throw an exception for next year.");
+        } catch (InvalidHITDateException e) {
+        }
+    } catch (DateDoesNotExistException e) {
+    }
+}
 }
