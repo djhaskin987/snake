ecf18d77c39e7cb52b801111c56831836027badf
Nathan Standiford <ns@byu.edu>
2013-09-28 14:25:49 -0600

    Added a few more barcode tests and methods

--- src/model/Barcode.java
+++ src/model/Barcode.java
@@ -3,23 +3,67 @@ package model;
 import java.io.Serializable;
 
 /**
- * The 	Barcode class represents a physical barcode that can be found on most purchased items
+ * The 	Barcode class represents a physical barcode that can be found on most purchased items.
+ * Barcode conforms to UPC-A standard (see http://en.wikipedia.org/wiki/Universal_Product_Code)
  * @author Kevin
  *
  */
 
-public class Barcode implements Serializable{
+public class Barcode implements Serializable {
 	private String barcode;
 	
 	/*
 	 * The constructor will make sure that the string passed in is a valid Barcode string
+	 * A valid Barcode string must be numeric and be 12 digits.
 	 */
-	public Barcode(String barcode){
+	public Barcode(String barcode) {
 		
 	}
 	
+	/**
+	 * Checks barcode string for conformity to UPC-A standard. That is, the input string
+	 * is 12 characters in length and numeric.
+	 * 
+	 * @pre string is not null
+	 * @param barcode the barcode string
+	 * @return true if barcode is valid
+	 */
+	public static boolean isValidBarcode(String barcode) {
+		return false;
+	}
+	
+	/**
+	 * Gets the barcode string.
+	 * @return the barcode string
+	 */
+	public String getBarcode() {
+		return null;
+	}
+	
+	/**
+	 * Sets the barcode string.
+	 * 
+	 * @pre barcode is not null and is a valid barcode
+	 * @param barcode the barcode string
+	 */
+	public void setBarcode(String barcode) {
+	}
 
-	public String toString(){
+	/**
+	 * @pre other is not null
+	 * @post 
+	 */
+	@Override
+	public boolean equals(Object other) {
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return -1;
+	}
+	
+	public String toString() {
 		return null;
 	}
 
---
+++ test-src/model/BarcodeTest.java
@@ -0,0 +1,54 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BarcodeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBarcode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBarcode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetBarcode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+}
