064a14269f65fac128a0d8f3bb2bbfce702619bd
Daniel Haskin <djhaskin987@gmail.com>
2013-09-30 12:12:57 -0600

    Add javadoc for non empty string

--- src/model/NonEmptyString.java
+++ src/model/NonEmptyString.java
@@ -4,8 +4,9 @@ import java.io.Serializable;
 /**
  * This is a Wrapper class to help enforce contracts throughout the program
  * that require non-empty strings
- * @author Kevin
- *
+ * @author Daniel Jay Haskin
+ * @invariant internal string value is immutable and can only be set
+ *   at construction time.
  */
 public class NonEmptyString implements Serializable {
 	/**
@@ -16,7 +17,10 @@ public class NonEmptyString implements Serializable {
 	
 	/**
 	 * The constructor ensures that the String is non-empty
+	 * @pre value is non-empty and non-null.
+	 * @post the NonEmptyString's value is set to the string given by the 'value' parameter.
 	 * @param value
+	 * @return void
 	 */
 	public NonEmptyString(String value){
 		if (value == null || value.equals(""))
@@ -26,18 +30,42 @@ public class NonEmptyString implements Serializable {
 		this.value = value;
 	}
 	
-	public String getValue(){
+	/** Returns the value of the NonEmptyString.
+	 * @pre The NonEmptyString is properly constructed.
+	 * @post The NonEmptyString remains unchanged.
+	 * @return the value fo the NonEmptyString.
+	 */
+	public final String getValue(){
 		return value;
 	}
 	
-	public String toString(){
+	/** Returns the value of the NonEmptyString.
+	 * @pre The NonEmptyString is properly constructed.
+	 * @post The NonEmptyString remains unchanged.
+	 * @return the value fo the NonEmptyString.
+	 */
+	public final String toString(){
 		return value;
 	}
 	
+	/** Returns the hash code fo the underlying string.
+	 * @pre The NonEmptyString is properly constructed.
+	 * @post The NonEmptyString remains unchanged.
+	 * @return The hash code of the non-empty string's value.
+	 */
 	@Override
 	public int hashCode(){
 		return getValue().hashCode();
 	}
+	
+	/** Returns whether the object 'other' and this object are equal.
+	 * This means they must both be NonEmptyStrings and have the same
+	 * underlying string value.
+	 * @pre The NonEmptyString is properly constructed. 'other' is not null.
+	 * @post The NonEmptyString remains unchanged.
+	 * @return Whether the two objects are instanceof NonEmptyString 
+	 *   and if their underlying values are equal.
+	 */
 	
 	@Override
 	public boolean equals(Object other)
