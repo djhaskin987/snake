ba13e3eb8df9c140378f9e10f13dda573f8f016e
Daniel Haskin <djhaskin987@gmail.com>
2013-10-11 12:57:37 -0600

    Add SelectState

---
+++ src/gui/common/SelectState.java
@@ -0,0 +1,43 @@
+package gui.common;
+
+public class SelectState
+{
+    private static SelectState instance;
+
+    public static SelectState getInstance()
+    {
+        if (instance != null)
+        {
+            instance = new SelectState(null);
+        }
+        return instance;
+    }
+
+    private Object selected;
+
+    private SelectState()
+    {
+        unselect();
+    }
+
+    public boolean isSomethingSelected()
+    {
+        return selected == null;
+    }
+
+    public Object getSelected()
+    {
+        return selected;
+    }
+
+    public void setSelected(Object & selected)
+    {
+        this.selected = selected;
+    }
+
+    public void unselect()
+    {
+        selected = null;
+    }
+}
+
