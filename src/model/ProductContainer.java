426f3c2f1c51a038f87ccd40c029dfb5580148d7
Daniel Haskin <djhaskin987@gmail.com>
2013-10-26 15:40:41 -0600

    Trailing whitespace

--- src/model/ProductContainer.java
+++ src/model/ProductContainer.java
@@ -8,16 +8,16 @@ import gui.common.*;
 /**
  * Abstract superclass of StorageUnit and ProductGroup.
  * Basically, it's for the classes that contain products, items, and ProductGroups.
- * 
+ *
  * @author Daniel Carrier
  *
  */
 public abstract class ProductContainer extends ModelObservable implements IProductContainer {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -7498283844315351927L;
-	
+
 	protected NonEmptyString name;
 	protected ProductItems productItems;
 	protected ProductContainers productContainers;
@@ -29,7 +29,7 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		productItems = new ProductItems((IProductContainer)this);
 		tagable = new Tagable();
 	}
-	
+
 	public ProductContainer()
 	{
 		this.name = new NonEmptyString("test");
@@ -41,41 +41,41 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 	public Collection<IProductContainer> getProductContainers() {
 		return productContainers.getProductContainers().values();
 	}
-	
+
 	public NonEmptyString getName() {
 		return name;
 	}
-	
+
 	/**
 	 * Retrieves a collection of products in the product group
-	 * 
+	 *
 	 * @return an unmodifiable Collection of Products
-	 * 
+	 *
 	 * {@pre productItems != null}
-	 * 
+	 *
 	 * {@post unmodifiable Collection of Products}
 	 */
 	public Collection<IProduct> getProducts()
 	{
 		return productItems.getProducts();
 	}
-	
-	
+
+
 	 /** retrieve a list of items based on the product name.
-	 * 
+	 *
 	 * @param productName the name of the product
-	 * 
-	 * @return list of items found 
-	 * 
+	 *
+	 * @return list of items found
+	 *
 	 * {@pre productName != null && productName != "" && productItems != null}
-	 * 
+	 *
 	 * {@post a list of items}
 	 */
     @Override
 	public Collection<IItem> getItems(IProduct p) {
 		return productItems.getItems(p);
 	}
-	
+
 	public void add(IItem item) {
 		IProduct product = item.getProduct();
 		for (IProductContainer productContainer : productContainers) {
@@ -88,13 +88,13 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		item.setProductContainer(this);
 		return;
 	}
-	
+
 	@Override
 	public boolean hasItems()
 	{
 		return productItems.getItems().size() > 0;
 	}
-	
+
 	@Override
 	public boolean hasItemsRecursive()
 	{
@@ -111,30 +111,30 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Checks to see if a product is in this ProductContainer
-	 * 
+	 *
 	 * @param product the product to search for
-	 * 
-	 * @return true if product is contained in the ProductContainer. false otherwise 
-	 * 
+	 *
+	 * @return true if product is contained in the ProductContainer. false otherwise
+	 *
 	 * {@pre productItems != null}
-	 * 
+	 *
 	 * {@post true if product is contained in the ProductContainer. False otherwise. }
 	 */
 	public boolean contains(IProduct product)
 	{
 		return productItems.contains(product);
 	}
-	
+
 	/**
 	 * remove item from ProductContainer by barcode
-	 * 
+	 *
 	 * @param barcode the barcode of item
-	 * 
+	 *
 	 * {@pre barcode != null && productItems != null}
-	 * 
+	 *
 	 * {@post item is removed from StorageUnit if Barcode exists}
 	 */
 	@Override
@@ -148,14 +148,14 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		}
 	}
 
-	
+
 	/**
 	 * Adds an Item to the ProductUnit
-	 * 
+	 *
 	 * @param item the Item to add
-	 * 
+	 *
 	 * {@pre item != null && productItems != null}
-	 * 
+	 *
 	 * {@post item added}
 	 */
 	@Override
@@ -163,14 +163,14 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		productItems.addItem(item);
 	}
 
-	
+
 	/**
 	 * Adds a batch to the ProductUnit
-	 * 
+	 *
 	 * @param item the Item to add
-	 * 
+	 *
 	 * {@pre item != null && productItems != null}
-	 * 
+	 *
 	 * {@post item added}
 	 */
 	@Override
@@ -182,9 +182,9 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 	 * Add ProductGroup to ProductContainer
 	 *
 	 * @param productGroup the ProductGroup to add
-	 * 
+	 *
 	 * {@pre productGroups != null && productGroup != null}
-	 * 
+	 *
 	 * {@post productGroup added}
 	 */
 	@Override
@@ -193,14 +193,14 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		productContainers.add(PC);
 	}
 
-	
+
 	/**
 	 * remove ProductContainer from ProductContainer
-	 * 
+	 *
 	 * @param name the name of the productContainer
-	 * 
+	 *
 	 * {@pre name != null && name != ""}
-	 * 
+	 *
 	 * {@post productContainer deleted if it exists}
 	 */
 	@Override
@@ -209,14 +209,14 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 	}
 
 	/**
-	 * Sets the product container 
-	 * 
+	 * Sets the product container
+	 *
 	 * @param name the product container name
-	 * 
+	 *
 	 * @param productContainer the product container
-	 * 
+	 *
 	 * {@pre name != null && productContainer != null && productContainer is a ProductGroup}
-	 * 
+	 *
 	 * {@post productContainer is set}
 	 */
 	@Override
@@ -224,20 +224,20 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 			ProductContainer productContainer) {
 		productContainers.setProductGroup(new NonEmptyString(name), (ProductGroup)productContainer);
 	}
-	
+
 	public void setName(String name){
 		this.name = new NonEmptyString(name);
 	}
 
 	/**
 	 * Transfers an item to another product container.
-	 * 
+	 *
 	 * @param item the item to transfer
-	 * 
+	 *
 	 * @param newProductContainer the ProductContainer to transfer the item to
-	 * 
+	 *
 	 * {@pre item != null && newProductContainer != null && productItems.contains(items)}
-	 * 
+	 *
 	 * {@post item is transferred}
 	 */
 	@Override
@@ -251,13 +251,13 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 
 	/**
 	 * Transfers a product to another product container.
-	 * 
+	 *
 	 * @param product the product to transfer
-	 * 
+	 *
 	 * @param newProductContainer the ProductContainer to transfer the item to
-	 * 
+	 *
 	 * {@pre product != null && newProductContainer != null && productItems.contains(product)}
-	 * 
+	 *
 	 * {@post product is transferred}
 	 */
 	@Override
@@ -271,7 +271,7 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		for (IItem i : itemsToTransfer) {
 			productItems.removeItem(i);
 			i.setProductContainer(newProductContainer);
-		}		
+		}
 		for (IItem i: itemsToTransfer) {
 			newProductContainer.add(i);
 		}
@@ -279,9 +279,9 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 
 	/**
 	 * Retrieves the product container from the children elements of the ProductGroup
-	 * 
+	 *
 	 * @param name 	the name of the product
-	 * 
+	 *
 	 * @return 		IProductContainer object containing Product with description of 'name'.
 	 * 				Null if there nothing is there.
 	 *
@@ -309,40 +309,40 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 			return child;
 		}
 	}
-	
+
 	public Object getTag()
 	{
 		return tagable.getTag();
 	}
-	
+
 	public void setTag(Object o)
 	{
 		tagable.setTag(o);
 	}
-	
-	
+
+
 	public boolean hasTag()
 	{
 		return tagable.hasTag();
 	}
-	
+
 	@Override
 	public boolean canAddItems()
 	{
 		return true;
 	}
-	
+
 	@Override
 	public boolean canRemoveItems()
 	{
 		return hasItemsRecursive();
 	}
-	
+
 	@Override
 	public boolean canDelete() {
 		return !hasItemsRecursive();
 	}
-	
+
 	@Override
 	public String getUnit()
 	{
@@ -356,7 +356,7 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 			return unit.getName().toString();
 		}
 	}
-	
+
 	@Override
 	public IProductContainer getUnitPC()
 	{
@@ -378,17 +378,17 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 			return child;
 		}
 	}
-	
+
 
 	/**
 	 * Used to display product group name in context panel.
 	 * Not all product containers have a product group.
 	 * By default, returns the empty string.
-	 * 
+	 *
 	 * @return null
-	 * 
+	 *
 	 * {@pre none}
-	 * 
+	 *
 	 * {@post none}
 	 * {@returns the empty string}
 	 */
@@ -396,16 +396,16 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 	public String getProductGroupName() {
 		return "";
 	}
-	
+
 	/**
 	 * Used to display three month supply in context panel.
 	 * Not all product containers have a three month supply.
 	 * By default, returns the empty string.
-	 * 
+	 *
 	 * @return null
-	 * 
+	 *
 	 * {@pre none}
-	 * 
+	 *
 	 * {@post none}
 	 * {@returns the empty string}
 	 */
@@ -413,19 +413,19 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 	public String getThreeMonthSupply() {
 		return "";
 	}
-	
+
 	@Override
 	public boolean hasChild(String pcName)
 	{
 		NonEmptyString key = new NonEmptyString(pcName);
 		return productContainers.getProductContainers().containsKey(key);
 	}
-	
+
 	@Override
 	public void removeProduct(IProduct product) {
 		productItems.removeProduct(product);
 	}
-	
+
 	@Override
 	public void removeProductRecursive(IProduct product) {
 		for(IProductContainer productContainer : productContainers) {
@@ -443,11 +443,11 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		}
 		return items;
 	}
-	
+
 	@Override
 	public Collection<IProduct> getProductsRecursive()
 	{
-		Collection<IProduct> returned = 
+		Collection<IProduct> returned =
 				getProducts();
 		for (IProductContainer pc : productContainers)
 		{
@@ -455,7 +455,7 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 		}
 		return returned;
 	}
-	
+
 	public void moveProduct(IProduct product, IProductContainer target)
 	{
 		if (product == null)
@@ -474,7 +474,7 @@ public abstract class ProductContainer extends ModelObservable implements IProdu
 			target.addItem(i);
 		}
 	}
-	
+
 	public void moveItem(IItem item, IProductContainer target)
 	{
 
