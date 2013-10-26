6c1ae91a11a1ecffbf6658969303dc4c5c6b0edb
DanielLC <daniel_l_c@yahoo.com>
2013-10-26 15:12:03 -0600

    Fixed some minor stuff.

--- src/gui/batches/AddItemBatchController.java
+++ src/gui/batches/AddItemBatchController.java
@@ -216,7 +216,7 @@ public class AddItemBatchController extends Controller implements
 			}
 		}
 		
-		int count = getItemCount();		
+		int count = getItemCount();
 		// create a list of items
 		ObservableArgs<IItem> items = new ObservableArgs<IItem>();
 		IItem lastItem = null;
@@ -239,6 +239,7 @@ public class AddItemBatchController extends Controller implements
 	
 	private void resetControls() {
 		IAddItemBatchView v = getView();
+		v.setBarcode("");
 		v.setCount("1");
 		v.setEntryDate(new java.util.Date());
 	}
