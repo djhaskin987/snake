cb882ffcdbbbcb7ca44a7ff29f4052c542b2bb89
DanielLC <daniel_l_c@yahoo.com>
2013-10-31 18:00:09 -0600

    I almost forgot to actually add the IModelTagable and ModelTagable files.

---
+++ src/model/IModelTagable.java
@@ -0,0 +1,20 @@
+package model;
+
+public interface IModelTagable {
+
+	/**
+	 * Returns the current value of the Tag property.
+	 * 
+	 */
+	public Object getTag();
+	/**
+	 * Sets the Tag property.
+	 * 
+	 * @param value new value for the Tag property
+	 */
+	public void setTag(Object o);
+	/**
+	 * Returns true if and only if Tag is not null.
+	 */
+	public boolean hasTag();
+}
