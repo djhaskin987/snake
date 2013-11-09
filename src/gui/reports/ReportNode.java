d166c7ed1fa3a4dfe90283c8e7e1f916bd244c19
Nathan Standiford <ns@byu.edu>
2013-11-09 10:53:38 -0700

    report

---
+++ src/gui/reports/ReportNode.java
@@ -0,0 +1,21 @@
+package gui.reports;
+
+/**
+ * The Report node is the root node. It is composed of a Heading and a body.
+ * 
+ * @author nstandif
+ *
+ */
+public class ReportNode implements Node {
+
+	public ReportNode() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void accept(ReportVisitor v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
