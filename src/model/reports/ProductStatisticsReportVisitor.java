0f80526b3c09637cb16e05cde2734cd01a1952f6
Nathan Standiford <ns@byu.edu>
2013-11-15 19:59:10 -0700

    Current Avg / Supply

--- src/model/reports/ProductStatisticsReportVisitor.java
+++ src/model/reports/ProductStatisticsReportVisitor.java
@@ -2,20 +2,27 @@ package model.reports;
 
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import model.DateTime;
 import model.Item;
+import model.NonEmptyString;
 import model.Product;
 import model.ProductGroup;
+import model.Quantity;
 import model.StorageUnit;
 import model.StorageUnits;
+import model.ValidDate;
 
 import model.reports.ReportBuilder;
 import model.reports.ReportVisitor;
+import org.apache.commons.lang3.time.DateUtils;
 
 class ProductStatisticsUtils {
 	public static String[] getRowHeader() {
@@ -24,49 +31,106 @@ class ProductStatisticsUtils {
 		return rowHeader;
 	}
 	
-	public static String[] getRow(Product p, List<Item> items, int months) {
+	public static String[] getRow(Product p, List<Item> allItems, List<Item> currentItems, List<Item> exitItems, int months) {
 		String description = getDescription(p);
 		String barcode = getBarcode(p);
 		String size = getSize(p);
 		String threeMonthSupply = getThreeMonthSupply(p);
-		String supplyCurAvg = getCurAvgSupply(items, months);
-		String supplyMinMax = getSupplyMinMax(items, months);
-		String supplyUsedAdded = getSupplyUsedAdded(items, months);
+		String supplyCurAvg = getCurAvgSupply(allItems, currentItems, months);
+		String supplyMinMax = getSupplyMinMax(allItems, months);
+		String supplyUsedAdded = getSupplyUsedAdded(allItems, months);
 		String shelfLife = getShelfLife(p);
-		String usedAgeAvgMax = getUsedAgeAvgMax(items, months);
-		String curAgeAvgMax = getCurAgeAvgMax(items, months);
+		String usedAgeAvgMax = getUsedAgeAvgMax(allItems, months);
+		String curAgeAvgMax = getCurAgeAvgMax(allItems, months);
 		String[] row = new String[] { description, barcode, size, threeMonthSupply, supplyCurAvg, supplyMinMax, supplyUsedAdded, shelfLife, usedAgeAvgMax, curAgeAvgMax };
 		return row;
 	}
 	
 	public static String getDescription(Product p) {
-		return "";
+		NonEmptyString neDescription = p.getDescription();
+		String description = neDescription.getValue();
+		return description;
 	}
 	
 	public static String getBarcode(Product p) {
-		return "";
+		NonEmptyString neBarcode = p.getBarcode();
+		return neBarcode.getValue();
 	}
 	
 	public static String getThreeMonthSupply(Product p) {
-		return "";
+		Integer iSupply = p.getThreeMonthSupply();
+		String supply = iSupply.toString();
+		return supply;
 	}
 	
 	public static String getSize(Product p) {
-		return "";
+		Quantity qItemSize = p.getItemSize();
+		String itemSize = qItemSize.getValueString();
+		return itemSize;
 	}
 	
-	public static String getCurAvgSupply(List<Item> items, int months) {
-		int cur = getCurrentSupply(items);
-		int avg = getAverageSupply(items, months);
-		return String.format("%s days / %s days", cur, avg);
+	public static String getCurAvgSupply(List<Item> allItems, List<Item> currentItems, int months) {
+		int cur = getCurrentSupply(currentItems);
+		double avg = getAverageSupply(allItems, months);
+		return String.format("%s days / %.2f days", cur, avg);
 	}
 	
-	public static int getCurrentSupply(List<Item> items) {
-		return -1;
+	public static int getCurrentSupply(List<Item> currentItems) {
+		return currentItems.size();
 	}
 	
-	public static int getAverageSupply(List<Item> items, int months) {
-		return -1;
+	
+	public static double getAverageSupply(List<Item> allItems, int months) {
+		long lifespans = 0;
+		for(Item item : allItems) {
+			lifespans += getLifeSpan(item, months);
+		}
+		int daysFromNow = getDaysFromNow(months);
+		return (double) lifespans / (double)daysFromNow;
+	}
+	
+	private static long getLifeSpan(Item item, int months) {
+		ValidDate mValidDate = item.getEntryDate();
+		Date date = mValidDate.toJavaUtilDate();
+		Calendar entry = Calendar.getInstance();
+		entry.setTime(date);
+		Calendar start = getCal(months);
+		start = getMostRecentDate(start, entry);
+		DateTime mExit = item.getExitTime();
+		Calendar exit;
+		if (mExit != null) {
+			exit = mExit.getCalendar();
+		} else {
+			exit = Calendar.getInstance();
+			exit.setTime(new Date());
+		}
+		long startMillis = start.getTimeInMillis();
+		long exitMillis = exit.getTimeInMillis();
+		int lifespan = (int) ((exitMillis - startMillis) / DateUtils.MILLIS_PER_DAY);
+		return lifespan;
+	}
+	
+	private static Calendar getMostRecentDate(Calendar cal1, Calendar cal2) {
+		if (cal1.compareTo(cal2) > 0)
+			return cal1;
+		else
+			return cal2;
+	}
+	
+	public static Calendar getCal(int months) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.MONTH, -1 * months);
+		return cal;
+	}
+	
+	public static int getDaysFromNow(int months) {
+		Calendar cal = getCal(months);
+		Calendar now = Calendar.getInstance();
+		now.setTime(new Date());
+		long diff = now.getTimeInMillis() - cal.getTimeInMillis();
+		int days = (int) (diff / DateUtils.MILLIS_PER_DAY);
+		return days;
 	}
 	
 	public static String getSupplyMinMax(List<Item> items, int months) {
@@ -98,7 +162,13 @@ class ProductStatisticsUtils {
 	}
 	
 	public static String getShelfLife(Product p) {
-		return "";
+		Integer iShelfLife = p.getShelfLife();
+		if (iShelfLife > 0) {
+		String shelfLife = iShelfLife.toString() + " months";
+		return shelfLife;
+		} else {
+			return "";
+		}
 	}
 	
 	public static String getUsedAgeAvgMax(List<Item> items, int months) {
@@ -133,11 +203,16 @@ class ProductStatisticsUtils {
 public class ProductStatisticsReportVisitor implements ReportVisitor {
 	private ReportBuilder builder;
 	private int months;
-	Map<Product, List<Item>> productItems;
+	Map<Product, List<Item>> allProductItems;
+	Map<Product, List<Item>> currentProductItems;
+	Map<Product, List<Item>> exitProductItems;
+	
 	public ProductStatisticsReportVisitor(ReportBuilder rb, int months) {
 		builder = rb;
 		this.months = months;
-		productItems = new HashMap<Product, List<Item>>();
+		allProductItems = new HashMap<Product, List<Item>>();
+		currentProductItems = new HashMap<Product, List<Item>>();
+		exitProductItems = new HashMap<Product, List<Item>>();
 	}
 
 	@Override
@@ -157,6 +232,18 @@ public class ProductStatisticsReportVisitor implements ReportVisitor {
 
 	@Override
 	public void visit(Item item) {
+		
+		if (isValid(item)) {
+			putItem(item, allProductItems);
+			if (item.getExitTime() != null) {
+				putItem(item, exitProductItems);
+			} else {
+				putItem(item, currentProductItems);
+			}
+		}
+	}
+	
+	public void putItem(Item item, Map<Product, List<Item>> productItems) {
 		Product p = (Product) item.getProduct();
 		List<Item> items = null;
 		if (productItems.containsKey(p)) {
@@ -168,10 +255,22 @@ public class ProductStatisticsReportVisitor implements ReportVisitor {
 		items.add(item);
 	}
 
+	private boolean isValid(Item item) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date());
+		cal.add(Calendar.MONTH, -1 * months);
+		DateTime mExitTime = item.getExitTime();
+		if (mExitTime != null) {
+			Calendar exitTime = mExitTime.getCalendar();
+			return cal.compareTo(exitTime) > 0;
+		}
+		else return true;
+	}
+	
 	@Override
 	public void visit(Product product) {
-		if (!productItems.containsKey(product))
-			productItems.put(product, new ArrayList<Item>());
+		if (!allProductItems.containsKey(product))
+			allProductItems.put(product, new ArrayList<Item>());
 	}
 
 	@Override
@@ -183,16 +282,18 @@ public class ProductStatisticsReportVisitor implements ReportVisitor {
 	}
 	
 	private String[][] compileTable() {
-		int size = productItems.size() + 1;
+		int size = allProductItems.size() + 1;
 		String[][] table = new String[size][];
 		table[0] = ProductStatisticsUtils.getRowHeader();
-		Collection<Product> products = productItems.keySet();
+		Collection<Product> products = allProductItems.keySet();
 		Iterator<Product> itr = products.iterator();
 		int i = 1;
-		while (itr.hasNext()){
+		while (itr.hasNext()) {
 			Product p = itr.next();
-			List<Item> items = productItems.get(p);
-			table[i] = ProductStatisticsUtils.getRow(p, items, months);
+			List<Item> allItems = allProductItems.get(p);
+			List<Item> currentItems = currentProductItems.get(p);
+			List<Item> exitItems = exitProductItems.get(p);
+			table[i] = ProductStatisticsUtils.getRow(p, allItems, currentItems, exitItems, months);
 		}
 		return table;
 	}
