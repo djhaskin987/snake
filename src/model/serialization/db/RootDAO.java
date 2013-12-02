a72f4d600f7af3ec67ef80db1a99e01eaaa81db2
Daniel Haskin <djhaskin987@gmail.com>
2013-12-02 15:29:13 -0700

    Some stuff

--- src/model/serialization/db/RootDAO.java
+++ src/model/serialization/db/RootDAO.java
@@ -1,22 +1,36 @@
 package model.serialization.db;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 import java.util.List;
 
 import model.IProductContainer;
+import model.RemovedItems;
 import model.StorageUnits;
 
 public class RootDAO implements IRootDAO {
 	private Connection dbConnection;
+	private Statement dbStatement;
 
 	public RootDAO(Connection dbConnection) {
 		this.dbConnection = dbConnection;
+		try {
+			dbStatement = dbConnection.createStatement();
+		} catch (SQLException e) {
+			System.err.println("Failed to create mechanism allowing the program" +
+					" to talk to the database");
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public List<StorageUnits> readAll() {
-		
-		return null;
+		List<StorageUnits> returned = new ArrayList<StorageUnits>(1);
+		returned.add(read(null));
+		return returned;
 	}
 
 	@Override
@@ -27,8 +41,25 @@ public class RootDAO implements IRootDAO {
 
 	@Override
 	public StorageUnits read(Object key) {
-		// TODO Auto-generated method stub
-		return null;
+		String dateName =
+				"dateSinceLastRemovedItemsReport";
+		ResultSet reportDateSet =
+				dbStatement.executeQuery("select * from Model where " +
+						+ "'name' = '" + dateName +
+						"';");
+		
+		reportDateSet.next();
+		RemovedItems removedItems = null;
+		Date dateSinceLastRemovedItemsReport = reportDateSet.getDate("value");
+		
+		StorageUnits returned = new StorageUnits();
+		returned
+			.setDateSinceLastRemovedItemsReport(
+					dateSinceLastRemovedItemsReport);
+		returned
+			.setTag(null);
+		returned
+			.setName(name);
 	}
 
 	@Override
