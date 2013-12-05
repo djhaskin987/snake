9e1afe82aa94cfba1ea04ce481e8fd573eaf4ae9
Daniel Haskin <djhaskin987@gmail.com>
2013-12-04 20:38:34 -0700

    Add stuff necessary for factual java plugin

--- src/plugins/DanHaskinProductIdentifier.java
+++ src/plugins/DanHaskinProductIdentifier.java
@@ -1,47 +1,39 @@
 package plugins;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import com.factual.driver.Factual;
+import com.factual.driver.Query;
+import com.factual.driver.ReadResponse;
 
 import model.productidentifier.AbstractProductIdentifier;
 
 public class DanHaskinProductIdentifier extends AbstractProductIdentifier {
+	private Factual factualConnection;
 
 	public DanHaskinProductIdentifier() {
+		factualConnection = new Factual(
+				"yFtEd817Gy55rmr508fYczhVQoff0GngVAY5Orji",
+				"k1z3e19c3opMpEDysMYURMSZPYDcdBlCmBWqEdg7");
 	}
 
 	@Override
 	public String _getProduct(String barcode) {
 		try {
-			URL src = getUrl(barcode);
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			Document doc = builder.parse(src.openStream());
-			NodeList nodes = doc.getElementsByTagName("product");
-			Node n = nodes.item(1);
-			String productName = n.getTextContent();
-			return productName;
+			ReadResponse read = factualConnection.fetch("products-cpg",
+					new Query().field("upc")
+						       .isEqual(barcode));
+			return read.first().get("product_name").toString();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-	private URL getUrl(String barcode) throws MalformedURLException {
-		String urlStr = String.format("http://eandata.com/feed/?v=3&keycode=419BA3F5529E2EE2&mode=xml&find=%s&get=product", barcode);
-		URL src = new URL(urlStr);
-		return src;
-	}
 	
 	public static void main(String [] args) {
 		DanHaskinProductIdentifier ident = new DanHaskinProductIdentifier();
-		String product = ident._getProduct("0049000006582");
-		System.out.println("0049000006582" + "=" + product);
+		String product = ident._getProduct("049000006582");
+		String otherProduct = ident._getProduct("052000131512");
+		System.out.println("049000006582" + " = " + product);
+		System.out.println("052000131512" + " = " + otherProduct);
 	}
 }
